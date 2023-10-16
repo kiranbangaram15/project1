c7e3fd2ac4bcc1c0a21670d1e1bd0b52afb58247
kiranbangaram15 <147645374+kiranbangaram15@users.noreply.github.com>
2023-10-16 10:42:40 +0530

    Add files via upload

---
+++ DAY3/index.js
@@ -0,0 +1,62 @@
+
+// prompt("Enter your name");
+// console.log("This is console");
+// console.log(6+5);
+// document.write("<h1> javascript </h1>");
+
+// var num1=5;
+// var num1=6; // we cxan redeclare the variable
+// num1=8; //we can reassign the variable
+// console.log(num1);
+
+// let num2=9
+// // let num2=10 // we cant redeclare the variable
+// num2=11 // we cant reassign the variable
+// console.log(num2);
+
+// const num3=12;
+// // const num=15;  we cant redeclare the variable//
+// // num3=15; we cant reassign the variable //
+// console.log(num3);
+
+// let a=5
+// let A=6
+// console.log(3**4)
+// let x=5;
+// x++
+// console.log(x);
+// x--
+// console.log(x);
+
+// let num1=5;
+// let num2="5"
+// console.log(num1==num2) // it gives true , as it cheks only the value , to check the datatype use ===//
+// console.log(num1===num2); 
+
+// function mul(n1,n2,n3,n4){
+//     return n1*n2*n3*n4;
+// }
+
+// document.write(mul(4,5,6,7));
+
+// function greet(){
+//     alert("hello ")
+// }
+
+
+// let age= 10;
+// if(age>=18)
+// {
+//     document.write("Elligible for vote")
+// }
+// else{
+//     document.write("not elligible")
+// }
+
+// 
+
+// let  el = document.getElementById("head").innerHTML;
+// console.log(el);
+
+let ele = document.querySelector("#head").innerHTML;
+console.log(el);
