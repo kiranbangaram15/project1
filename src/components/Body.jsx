e21c666a0a11436a4e200fff4987924177b1fda2
kiranbangaram15 <147645374+kiranbangaram15@users.noreply.github.com>
2023-10-20 12:43:37 +0530

    Add files via upload

---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Body = () => {
+    return(
+    <>
+    <hr/>
+    <h3>About me</h3>
+ <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis maxime fugiat tempora esse voluptate, repellat ea impedit, inventore doloremque nemo iste in at sapiente ipsam ipsa, deleniti alias. Atque, dolorum.</p>   
+</>
+);
+};
+export default Body;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+    return(
+        <div id="box">
+            <h1>subhash chandra bose</h1>
+            <img 
+            id="image"
+            src="https://cdn.britannica.com/37/171337-004-0B980E9D/Subhas-Chandra-Bose-Indian-National-Army.jpg" alt="loading...."/>
+        </div>
+    );
+}
+export default Header;
